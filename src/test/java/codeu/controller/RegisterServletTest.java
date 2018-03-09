d890fc86f60c49ad775cf916552706578d4c199d
Silvie Coheleach <silviec98@gmail.com>
2018-03-09 13:46:48 -0500

    adjusted tabbing as to kevin's comments

--- src/test/java/codeu/controller/RegisterServletTest.java
+++ src/test/java/codeu/controller/RegisterServletTest.java
@@ -11,26 +11,26 @@ import org.mockito.Mockito;
 
 public class RegisterServletTest 
 {
-	private RegisterServlet registerServlet;
-	private HttpServletRequest mockRequest;
-	private PrintWriter mockPrintWriter;
-	private HttpServletResponse mockResponse;
-	
-	@Before
-	public void setup() throws IOException
-	{
-		registerServlet = new RegisterServlet();
-		mockRequest = Mockito.mock(HttpServletRequest.class);
-		mockPrintWriter = Mockito.mock(PrintWriter.class);
-		mockResponse = Mockito.mock(HttpServletResponse.class);
-		Mockito.when(mockResponse.getWriter()).thenReturn(mockPrintWriter);
-	}
-	
-	@Test
-	public void testDoGet() throws IOException, ServletException 
-	{
-		registerServlet.doGet(mockRequest, mockResponse);
-		Mockito.verify(mockPrintWriter).println("<h1>RegisterServlet GET "
-				+ "request.</h1>");
-	}
+  private RegisterServlet registerServlet;
+  private HttpServletRequest mockRequest;
+  private PrintWriter mockPrintWriter;
+  private HttpServletResponse mockResponse;
+
+  @Before
+  public void setup() throws IOException
+  {
+    registerServlet = new RegisterServlet();
+    mockRequest = Mockito.mock(HttpServletRequest.class);
+    mockPrintWriter = Mockito.mock(PrintWriter.class);
+    mockResponse = Mockito.mock(HttpServletResponse.class);
+    Mockito.when(mockResponse.getWriter()).thenReturn(mockPrintWriter);
+  }
+
+  @Test
+  public void testDoGet() throws IOException, ServletException 
+  {
+    registerServlet.doGet(mockRequest, mockResponse);
+    Mockito.verify(mockPrintWriter).println("<h1>RegisterServlet GET "
+      + "request.</h1>");
+  }
 }
