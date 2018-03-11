d89146546ffcbdf201a4d575d8fc3d2be65b2e9f
Ivy Wang <jwang28@nd.edu>
2018-03-10 21:52:09 -0500

    Replaced tabs with spaces

--- src/main/java/codeu/model/store/persistence/PersistentDataStore.java
+++ src/main/java/codeu/model/store/persistence/PersistentDataStore.java
@@ -64,7 +64,7 @@ public class PersistentDataStore {
       try {
         UUID uuid = UUID.fromString((String) entity.getProperty("uuid"));
         String userName = (String) entity.getProperty("username");
-		String password = (String)entity.getProperty("password");
+        String password = (String)entity.getProperty("password");
         Instant creationTime = Instant.parse((String) entity.getProperty("creation_time"));
         User user = new User(uuid, userName, password, creationTime);
         users.add(user);
